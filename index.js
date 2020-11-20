cac0f0c5c45f24e0087d437a7d6b1e6c64f9cd21
Adam Morris <amorris2003@gmail.com>
2020-11-21 09:57:25 +1000

    on message

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+client.login('Nzc5MjM5MTI4Mzk1MDIyMzM2.X7do_Q.wgUQFXT5uHgswh1HC33ijnceLPw');
 
 const Keyv = require('keyv');
 
@@ -7,12 +9,27 @@ const keyv = new Keyv();
 keyv.on('error', err => console.error('Keyv connection error:', err));
 
 let botTriviaChannel;
+let botTriviaQuestions;
+const botTriviaTimeout = 1000;
 
+// After the bot is logged in and ready
 client.once('ready', () => {
   // Gets the #bot-trivia channel
   botTriviaChannel = client.channels.cache.get('779241835649957939');
-  botTriviaChannel.send('hi');
+
+  // Starts sending questions to #bot-trivia
+  botTriviaQuestions = setInterval(botTriviaNewQuestion, botTriviaTimeout);
 });
 
+client.on('message', msg => {
+  if (msg.content == 'hi') {
+    clearInterval(botTriviaQuestions);
+    botTriviaChannel.send('hey');
+  }
+});
 
-client.login('Nzc5MjM5MTI4Mzk1MDIyMzM2.X7do_Q.wgUQFXT5uHgswh1HC33ijnceLPw');
+const botTriviaNewQuestion = () => {
+  botTriviaChannel.send('hi');
+};
+
+
