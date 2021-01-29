f9c9c13e2f543eca2eecb0ea776c3c7d972b102b
Adam Morris <aimorris@aimorris.com>
2021-01-29 13:07:18 +1000

    more questions

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ const questions = [
   {
     'question': '7-Eleven stores were temporarily converted into Kwik E-marts to promote the release of what movie?',
     'answer': ['the simpsons', 'simpsons', 'simpsons movie', 'the simpsons movie'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'On average, what denomination of U.S. paper currency remains in circulation the longest?',
@@ -22,62 +22,127 @@ const questions = [
   {
     'question': 'In the Simpsons, what member of the Simpson family is commonly seen sucking on a pacifier?',
     'answer': ['maggie', 'maggie simpsons', 'margaret'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'In the act that incited WWI, Serbian Gavrilo Princip assassinated Archduke Franz Ferdinand in 1914 in what city?',
     'answer': ['sarajevo'],
-    'category': 'humanities',
+    'category': 'Society & Humanities',
   },
   {
     'question': 'How old was Daniel Radcliffe when the first Harry Potter film came out?',
     'answer': ['12', 'twelve'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'Due to the popularity of Harry Potter, what word was added to the Oxford English dictionary in 2003?',
     'answer': ['muggle'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'In the Harry Potter series, what school does Harry Potter attend?',
     'answer': ['hogwarts', 'hogwarts school of witchcraft and wizardry'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'What is the fourth book in the Harry Potter series?',
     'answer': ['goblet of fire', 'harry potter and the goblet of fire', 'the goblet of fire'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'What chemical element has the symbol Pr?',
     'answer': ['praseodymium'],
-    'category': 'science',
+    'category': 'Science & Geography',
   },
   {
     'question': 'Who was the first United States Secretary of State?',
     'answer': ['thomas jefferson', 'jefferson'],
-    'category': 'humanities',
+    'category': 'Society & Humanities',
   },
   {
     'question': 'Which actor played Hannibal Lecter in the Hannibal movie trilogy?',
     'answer': ['sir anthony hopkins', 'hopkins', 'sir anthony'],
-    'category': 'entertainment',
+    'category': 'Arts & Entertainment',
   },
   {
     'question': 'Margaret Thatcher was the prime minister of which country?',
     'answer': ['united kingdom', 'uk'],
-    'category': 'humanities',
+    'category': 'Society & Humanities',
   },
   {
     'question': 'What was the legal first name of King George VI?',
     'answer': ['albert'],
-    'category': 'humanities',
+    'category': 'Society & Humanities',
   },
   {
     'question': 'What is the regnal name of the first monarch to abdicate?',
     'answer': ['king edward viii', 'edward viii', 'king edward 8th', 'king edward eighth', 'edward eighth', 'king edward 8', 'edward 8', 'edward eight'],
-    'category': 'humanities',
+    'category': 'Society & Humanities',
+  },
+  {
+    'question': 'On the children’s TV "Dora the Explorer", what is the name of the Dora\'s friend monkey?',
+    'answer': ['boots'],
+    'category': 'Arts & Entertainment',
+  },
+  {
+    'question': 'In the classic tale "Moby Dick", Moby Dick is what kind of whale?',
+    'answer': ['sperm', 'sperm whale'],
+    'category': 'Arts & Entertainment',
+  },
+  {
+    'question': 'What is pluviophobia the fear of?',
+    'answer': ['rain', 'storms', 'storm', 'lightning', 'thunder'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'What was the first zeppelin made of?',
+    'answer': ['aluminum', 'aluminium'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'Which major river flows through Gloucester?',
+    'answer': ['severn'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'In the show "American Dad", character Steve Smith attends what school?',
+    'answer': ['pearl bailey', 'pearl bailey school', 'pearl bailey high', 'pearl bailey high school'],
+    'category': 'Arts & Entertainment',
+  },
+  {
+    'question': 'What software company shares its name with a building material made from earthen materials?',
+    'answer': ['adobe'],
+    'category': 'Society & Humanities',
+  },
+  {
+    'question': 'What is the name of the longest river in the Western Hemisphere?',
+    'answer': ['amazon', 'the amazon', 'amazon river', 'the amazon river'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'What does an anemometer measure?',
+    'answer': ['wind', 'wind speed', 'wind velocity', 'wind direction'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'What is the capital of Finland?',
+    'answer': ['helsinki'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'What country has the same name as a bird?',
+    'answer': ['turkey'],
+    'category': 'Science & Geography',
+  },
+  {
+    'question': 'What colour is The Arts and Literature wedge in Trivial Pursuit?',
+    'answer': ['brown'],
+    'category': 'Arts & Entertainment',
+  },
+  {
+    'question': 'What is the capital of Tuvalu?',
+    'answer': ['funafuti'],
+    'category': 'Arts & Entertainment',
   },
 ];
 
